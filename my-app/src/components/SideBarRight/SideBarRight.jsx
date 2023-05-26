dc86696e0f361b4367dd322ae89777e17d4f3199
Maksim Bekis <maxamax997@gmail.com>
2023-05-26 16:12:55 +0300

    ready sideBarRight__cardTwo

--- my-app/src/components/SideBarRight/SideBarRight.jsx
+++ my-app/src/components/SideBarRight/SideBarRight.jsx
@@ -1,10 +1,15 @@
-
 import React from "react";
-import calendar from'../../svg/calendarTwo.svg'
+import calendar from "../../svg/calendarTwo.svg";
+import water from "../../svg/water.svg";
+import bottle from "../../png/bottle.png";
+import shadow from "../../svg/shadow.svg";
+import circle from "../../svg/circle.svg";
+import vectorOne from "../../svg/vector-one.svg";
+import vectorTwo from "../../svg/vector-two.svg";
 
 function SideBarRight() {
   return (
-    <div className="sideBarRight">
+    <aside className="sideBarRight">
       <button>
         <img src={calendar} /> Tue, 24 Nov 2021{" "}
         <svg
@@ -22,10 +27,186 @@ function SideBarRight() {
           />
         </svg>
       </button>
-      <div></div>
-    </div>
+      <div className="sideBarRight__cardOne">
+        <div className="sideBarRight__cardOne__SVG">
+          <img src={bottle} alt="" />
+          <img src={water} alt="" />
+          <img src={shadow} alt="" />
+          <img src={circle} alt="" />
+          <img src={vectorOne} alt="" />
+          <img src={vectorTwo} alt="" />
+        </div>
+        <span>Stay Hydrated and beat heat.</span>
+      </div>
+      <div className="sideBarRight__cardTwo">
+        <div className="sideBarRight__cardTwo__SVG">
+          <svg
+            width="113"
+            height="32"
+            viewBox="0 0 113 32"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              opacity="0.2"
+              d="M1 30.9124C9.14881 31.4293 29.675 29.9044 46.5893 19.6698C67.7321 6.87651 83.5893 0.286032 112 1.06138"
+              stroke="url(#paint0_linear_150_494)"
+              stroke-width="1.30286"
+              stroke-linecap="round"
+            />
+            <defs>
+              <linearGradient
+                id="paint0_linear_150_494"
+                x1="56.5"
+                y1="1"
+                x2="56.5"
+                y2="31"
+                gradientUnits="userSpaceOnUse"
+              >
+                <stop stop-color="white" />
+                <stop offset="1" stop-color="white" stop-opacity="0" />
+              </linearGradient>
+            </defs>
+          </svg>
+          <svg
+            width="149"
+            height="40"
+            viewBox="0 0 149 40"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              opacity="0.2"
+              d="M1.00006 38.9441C9.16673 39.464 43.8673 36.4825 57.946 24.7828C79.1352 7.17421 119.527 0.279176 148 1.05904"
+              stroke="url(#paint0_linear_150_495)"
+              stroke-width="1.30286"
+              stroke-linecap="round"
+            />
+            <defs>
+              <linearGradient
+                id="paint0_linear_150_495"
+                x1="74.5001"
+                y1="1"
+                x2="74.5001"
+                y2="39"
+                gradientUnits="userSpaceOnUse"
+              >
+                <stop stop-color="white" />
+                <stop offset="1" stop-color="white" stop-opacity="0" />
+              </linearGradient>
+            </defs>
+          </svg>
+          <svg
+            width="36"
+            height="36"
+            viewBox="0 0 36 36"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g clip-path="url(#clip0_150_501)">
+              <path
+                d="M8.45401 9.95384L18 0.407837L27.546 9.95384C29.434 11.8419 30.7197 14.2473 31.2406 16.8661C31.7615 19.4848 31.4942 22.1992 30.4724 24.666C29.4506 27.1328 27.7202 29.2412 25.5002 30.7246C23.2801 32.208 20.67 32.9997 18 32.9997C15.33 32.9997 12.7199 32.208 10.4998 30.7246C8.27976 29.2412 6.54944 27.1328 5.52765 24.666C4.50586 22.1992 4.23851 19.4848 4.75939 16.8661C5.28028 14.2473 6.56601 11.8419 8.45401 9.95384Z"
+                fill="url(#paint0_linear_150_501)"
+                fill-opacity="0.24"
+              />
+              <path
+                opacity="0.18"
+                d="M19.9286 6.42822C21.6429 7.92822 25.0714 11.0568 25.0714 11.5711"
+                stroke="url(#paint1_linear_150_501)"
+                stroke-width="1.30286"
+                stroke-linecap="round"
+              />
+            </g>
+            <defs>
+              <linearGradient
+                id="paint0_linear_150_501"
+                x1="18"
+                y1="0.407837"
+                x2="18"
+                y2="32.9997"
+                gradientUnits="userSpaceOnUse"
+              >
+                <stop stop-color="white" />
+                <stop offset="1" stop-color="white" stop-opacity="0" />
+              </linearGradient>
+              <linearGradient
+                id="paint1_linear_150_501"
+                x1="22.5"
+                y1="6.42822"
+                x2="22.5"
+                y2="11.5711"
+                gradientUnits="userSpaceOnUse"
+              >
+                <stop stop-color="white" />
+                <stop offset="1" stop-color="white" stop-opacity="0" />
+              </linearGradient>
+              <clipPath id="clip0_150_501">
+                <rect width="36" height="36" fill="white" />
+              </clipPath>
+            </defs>
+          </svg>
+          <svg
+            width="66"
+            height="40"
+            viewBox="0 0 66 40"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              opacity="0.2"
+              d="M1.29614 38.606C2.72544 31.006 10.3007 15.2849 29.1674 13.2003C48.0341 11.1158 60.0633 7.5 64.4515 1.5"
+              stroke="url(#paint0_linear_150_499)"
+              stroke-width="1.30286"
+              stroke-linecap="round"
+            />
+            <defs>
+              <linearGradient
+                id="paint0_linear_150_499"
+                x1="29.5247"
+                y1="4.08032"
+                x2="29.5247"
+                y2="38.606"
+                gradientUnits="userSpaceOnUse"
+              >
+                <stop stop-color="white" />
+                <stop offset="1" stop-color="white" stop-opacity="0" />
+              </linearGradient>
+            </defs>
+          </svg>
+          <svg
+            width="58"
+            height="37"
+            viewBox="0 0 58 37"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              opacity="0.2"
+              d="M0.727966 35.8171C2.15726 28.2171 9.73252 12.496 28.5992 10.4114C47.4659 8.32681 55.5176 3.46281 57.1851 1.29138"
+              stroke="url(#paint0_linear_150_500)"
+              stroke-width="1.30286"
+              stroke-linecap="round"
+            />
+            <defs>
+              <linearGradient
+                id="paint0_linear_150_500"
+                x1="28.9565"
+                y1="1.29138"
+                x2="28.9565"
+                y2="35.8171"
+                gradientUnits="userSpaceOnUse"
+              >
+                <stop stop-color="white" />
+                <stop offset="1" stop-color="white" stop-opacity="0" />
+              </linearGradient>
+            </defs>
+          </svg>
+        </div>
+        <h5>Intake Goal</h5>
+        <span>18000 ml / 25000 ml</span>
+      </div>
+    </aside>
   );
-
-} 
+}
 
 export default SideBarRight;
